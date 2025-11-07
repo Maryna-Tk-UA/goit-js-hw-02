cd19a9ec73795627b62471677dd9a2dc09f2b6ed
Maryna Tkachenko <marynapanchenko19@gmail.com>
2025-11-07 22:24:03 +0200

    change task-1

--- js/task-1.js
+++ js/task-1.js
@@ -2,8 +2,8 @@ function makeTransaction(quantity, pricePerDroid, customerCredits) {
   const totalPrice = quantity * pricePerDroid;
   if (customerCredits >= totalPrice) {
     return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
-  } else {
-    return "Insufficient funds!";
+    // } else {
+    //   return "Insufficient funds!";
   }
 }
 
